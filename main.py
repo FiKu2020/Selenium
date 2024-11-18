63f57a403e5fe6241841ae617950945cf300c96d
FiKu2020 <filip2006kujawa@gmail.com>
2024-11-18 18:41:34 +0100

    wyb jez + klik ciaskta

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,6 +13,16 @@ try:
         EC.element_to_be_clickable((By.CSS_SELECTOR, "button.fc-button.fc-primary-button"))
     )
     zgoda.click()
+    wyb_jezyk = wait.until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR,"div.langSelectButton#langSelect-EN"))
+    )
+    wyb_jezyk.click()
+    przycisk_ciastka = wait.until(
+        EC.element_to_be_clickable((By.ID, "bigCookie"))
+    )
+    while True:
+        przycisk_ciastka.click()
+        time.sleep(0.1)
 finally:
-    driver.quit()
-print("jest git")
+    # driver.quit()
+    print("jest git")
