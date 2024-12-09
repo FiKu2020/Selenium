82d2b63326f7218ee31c9ab1fa4ed010c172f64b
FiKu2020 <filip2006kujawa@gmail.com>
2024-12-09 20:03:17 +0100

    slow progress

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
+
 try:
+    # user_input = input()
+    # if input == "kill":
+    #     driver.quit()
     driver.get("https://orteil.dashnet.org/cookieclicker/")
     wait = WebDriverWait(driver, 10)
     zgoda = wait.until(
@@ -14,24 +17,21 @@ try:
     )
     zgoda.click()
     wyb_jezyk = wait.until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR,"div.langSelectButton#langSelect-EN"))
+        EC.element_to_be_clickable((By.CSS_SELECTOR, "div.langSelectButton#langSelect-EN"))
     )
     wyb_jezyk.click()
-    przycisk_ciastka = wait.until(
+    przycisk_ciastka = WebDriverWait(driver, 30).until(
         EC.element_to_be_clickable((By.ID, "bigCookie"))
     )
-    while True:
-        przycisk_ciastka.click()
-        time.sleep(0.1)
-finally:
-    # driver.quit()
-    print("jest git")
-    driver.quit()
 
-    def optimal_price():
-        wydajnosc_wszystkich_bud = []
-        for i in range(20):
-            cps_budynku = driver.execute_script(f'return Game.ObjectsById[{i}].storedCps')
-            cena_budynku = driver.execute_script(f"return Game.ObjectsById[{i}].price")
-            wydajnosc_danego_bud = cps_budynku / cena_budynku
-            wydajnosc_wszystkich_bud.append(wydajnosc_danego_bud)
+    while True:
+        try:
+            przycisk_ciastka = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.ID, "bigCookie"))
+            )
+            przycisk_ciastka.click()
+        except Exception as e:
+            print(f"cos walnelo: {e}")
+        #time.sleep(0.1)
+finally:
+    driver.quit()
